f752728a825d1bf85d459e803ec72094f698184d
gy22122004-forge <gy22122004@gmail.com>
2026-01-11 10:24:51 +0530

    Remove unnecessary whitespace in build.sh

--- gaurav_port/build.sh
+++ gaurav_port/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
-
 echo "Downloading Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:$(pwd)/flutter/bin"
-
 echo "Entering Flutter project directory..."
 cd gaurav_port
 
