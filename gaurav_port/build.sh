f70d27248d50a100d6d797b4e0dc5f2f4c6cdcdf
gy22122004-forge <gy22122004@gmail.com>
2026-01-11 10:23:52 +0530

    Update build script to include project directory change

--- gaurav_port/build.sh
+++ gaurav_port/build.sh
@@ -3,8 +3,10 @@ set -e
 
 echo "Downloading Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
-
 export PATH="$PATH:$(pwd)/flutter/bin"
+
+echo "Entering Flutter project directory..."
+cd gaurav_port
 
 echo "Flutter version:"
 flutter --version
