135b9ebcd4ad6f25f6e463dd816899884c5ceb2b
gy22122004-forge <gy22122004@gmail.com>
2026-01-11 10:18:00 +0530

    Update build script for Flutter installation

--- gaurav_port/build.sh
+++ gaurav_port/build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-set -e
+
 echo "Downloading Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:$(pwd)/flutter/bin"
-
-echo "Flutter version:"
-flutter --version
-
-echo "Enabling Flutter Web..."
-flutter config --enable-web
-
-echo "Fetching dependencies..."
-flutter pub get
+export PATH="$PATH:`pwd`/flutter/bin"
 
 echo "Flutter verification..."
 flutter doctor -v
