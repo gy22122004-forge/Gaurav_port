50308521a50ba299f913ad0606936b03950befb9
gy22122004-forge <gy22122004@gmail.com>
2026-01-11 10:25:51 +0530

    Update build script to download Flutter

--- gaurav_port/build.sh
+++ gaurav_port/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+
 echo "Downloading Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:$(pwd)/flutter/bin"
+
 echo "Entering Flutter project directory..."
 cd gaurav_port
 
